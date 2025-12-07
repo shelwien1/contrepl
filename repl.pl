b5659fe230548b4dd1200be615c70dca3745d948
shelwien1 <44040837+shelwien1@users.noreply.github.com>
2025-12-07 22:34:24 +0200

    Add files via upload

--- repl.pl
+++ repl.pl
@@ -1,93 +1,135 @@
-#perl
+#!/usr/bin/perl
+# repl.pl - Forward replacement with flag generation for reversibility
+use strict;
+use warnings;
+use re 'eval';
 
-# perl repl.pl book1.cfg book1 book1
+die "Usage: $0 config input output flags\n" unless @ARGV == 4;
+my ($cfg_file, $in_file, $out_file, $flg_file) = @ARGV;
 
-undef $/;
-open( CFG, "<$ARGV[0]" ) || die;
-$cfg = <CFG>;
-close CFG;
-
-$cfg =~ s/\r//g;
-@c = split /\n/, $cfg;
-
-$u0 = $c[0];
-$u1 = $c[1]; # (?=$u1)
-eval "\$u = \"$c[2]\";";
-eval "\$v = \"$c[3]\";";
-
-print "$u0\n$u1\n<$u>\n<$v>\n\n";
-
-open( I, "<$ARGV[1]" ) || die;
-$a = <I>;
-close I;
-
-open( F, ">$ARGV[2].flg" ) || die;
-open( R, ">$ARGV[2].out" ) || die;
-open( L, ">$ARGV[2].log" ) || die;
-#open( B, ">book1.rst" ) || die;
-
-
-$a0 = $a;
-#$flag = "-" x (length($a)*2);
-undef @fl;
-$p0 = 0; # initial pos
-$p = 0; # current pos in output string
-
-sub func {                  
-  $p += length($`)-$p0;
-#  substr($flag,$p,1)='1';
-  $fl[$p]=1;
-
-  $p += length($v);
-  $p0 = length($`)+length($&);
+sub decode_escapes {
+    my $s = shift;
+    $s =~ s/\\x([0-9A-Fa-f]{2})/chr(hex($1))/ge;
+    $s =~ s/\\t/\t/g;
+    $s =~ s/\\n/\n/g;
+    $s =~ s/\\r/\r/g;
+    $s =~ s/\\\\/\\/g;
+    return $s;
 }
 
-sub func2 {
-  $pp = length($`);
+open my $cfg, '<:raw', $cfg_file or die "Cannot open $cfg_file: $!";
+my $cfg_data = do { local $/; <$cfg> };
+close $cfg;
 
-  $cc = $fl[$pp];
+$cfg_data =~ s/\r\n/\n/g;
+my @lines = split /\n/, $cfg_data;
 
-  if( not defined $cc ) { $cc=$fl[$pp]=0; };
+my $lb = shift @lines // '';
+my $la = shift @lines // '';
 
-#  return 1  if $fl[$pp]==1;
+my (%forward, %backward);
+my (@forward_keys, @backward_keys);
 
-#  $fl[$pp] = 0;
-
-#  if( substr($flag,length($`),1) eq '1' ) {
-#    return 1;
-#  }
-#  substr($flag,length($`),1) = '0';
-  return 0;
+for my $line (@lines) {
+    next if $line eq '' || $line !~ /\t/;
+    my ($from, $to) = split /\t/, $line, 2;
+    next unless defined $from && defined $to;
+    
+    $from = decode_escapes($from);
+    $to = decode_escapes($to);
+    
+    $forward{$from} = $to;
+    push @forward_keys, $from;
+    
+    unless (exists $backward{$to}) {
+        $backward{$to} = $from;
+        push @backward_keys, $to;
+    }
 }
 
-print "Forward replacement (book1->book1.out+book1.flg)\n";
+die "No replacement pairs found in config\n" unless @forward_keys;
 
-eval "\$a=~ s/(?<=$u0)$u(?=$u1)(?{func()})/$v/g;";
-$l = length($a);
+my $fwd_alt = join '|', map { quotemeta } sort { length($b) <=> length($a) } @forward_keys;
+my $bwd_alt = join '|', map { quotemeta } sort { length($b) <=> length($a) } @backward_keys;
 
-print "Backward replacement (book1.out+book1.flg->book1.rst)\n";
+# Read input
+open my $in, '<:raw', $in_file or die "Cannot open $in_file: $!";
+my $original = do { local $/; <$in> };
+close $in;
 
-print R $a;
+# Forward replacement with position mapping
+my @orig_pos;
+my $intermediate = '';
+my $last_end = 0;
 
-eval "\$a=~ s/(?<=$u0)$v(?=$u1)(?(?{func2()})|(*FAIL))/$u/g;";
-
-#print B $a;
-
-print "Filtering flags\n";
-#$flag = substr($flag,0,$l); # cut to output string length
-
-$flag="";
-for( $i=0; $i<$l; $i++ ) {
-  $c = $fl[$i];
-  $flag .= (defined $c) ? chr(48+$c) : '-';
+my $fwd_re = qr/(?<=$lb)($fwd_alt)(?=$la)/s;
+while ($original =~ /$fwd_re/g) {
+    my ($start, $end, $match) = ($-[0], $+[0], $1);
+    if ($start > $last_end) {
+        $intermediate .= substr($original, $last_end, $start - $last_end);
+        push @orig_pos, ($last_end .. $start - 1);
+    }
+    my $repl = $forward{$match};
+    $intermediate .= $repl;
+    push @orig_pos, ($start) x length($repl);
+    $last_end = $end;
+}
+if ($last_end < length($original)) {
+    $intermediate .= substr($original, $last_end);
+    push @orig_pos, ($last_end .. length($original) - 1);
 }
 
-$plog = $flag;
-$plog =~ tr/[01]/1/;
-$plog =~ tr/\-/0/;
-print L $plog;
+# Collect all backward matches using (*FAIL) trick
+my @bwd_matches;
+my %seen_pos;
 
-$flag =~ s/\-//g;
-print F $flag;
+sub collect_match {
+    return if $seen_pos{$-[0]}++;
+    push @bwd_matches, [$-[0], $1];
+}
 
+eval "\$intermediate =~ /(?<=$lb)($bwd_alt)(?=$la)(?{ collect_match() })(*FAIL)/gs;";
 
+# Compute preliminary flags (would this restore original?)
+my @preliminary;
+for my $m (@bwd_matches) {
+    my ($start, $match) = @$m;
+    my $repl = $backward{$match};
+    my $orig_start = $orig_pos[$start];
+    my $orig_substr = substr($original, $orig_start, length($repl));
+    push @preliminary, ($orig_substr eq $repl) ? 1 : 0;
+}
+
+# Simulate left-to-right replacement, computing position offsets
+my @final_flags = (0) x scalar(@bwd_matches);
+my $simulated = $intermediate;
+my $offset = 0;
+
+for my $i (0 .. $#bwd_matches) {
+    next unless $preliminary[$i];
+    
+    my ($orig_pos, $match) = @{$bwd_matches[$i]};
+    my $adj_pos = $orig_pos + $offset;
+    my $match_len = length($match);
+    
+    my $current = substr($simulated, $adj_pos, $match_len);
+    if ($current eq $match) {
+        my $repl = $backward{$match};
+        substr($simulated, $adj_pos, $match_len) = $repl;
+        $offset += length($repl) - $match_len;
+        $final_flags[$i] = 1;
+    }
+}
+
+my $flags = join '', @final_flags;
+
+# Write outputs
+open my $out, '>:raw', $out_file or die "Cannot open $out_file: $!";
+print $out $intermediate;
+close $out;
+
+open my $flg, '>:raw', $flg_file or die "Cannot open $flg_file: $!";
+print $flg $flags;
+close $flg;
+
+print STDERR "Original: ", length($original), " bytes, Output: ", length($intermediate), " bytes, Flags: ", length($flags), "\n";
--- replu.pl
+++ replu.pl
@@ -1,55 +1,99 @@
-#perl
+#!/usr/bin/perl
+# replu.pl - Reverse replacement using flags for exact restoration
+use strict;
+use warnings;
+use re 'eval';
 
-# perl replu.pl book1.cfg book1.out book1.flg book1.rst book1.logr
+die "Usage: $0 config input output flags\n" unless @ARGV == 4;
+my ($cfg_file, $in_file, $out_file, $flg_file) = @ARGV;
 
-undef $/;
-open( CFG, "<$ARGV[0]" ) || die;
-$cfg = <CFG>;
-close CFG;
-
-$cfg =~ s/\r//g;
-@c = split /\n/, $cfg;
-
-$u0 = $c[0];
-$u1 = $c[1]; # (?=$u1)
-eval "\$u = \"$c[2]\";";
-eval "\$v = \"$c[3]\";";
-
-print "$u0\n$u1\n<$u>\n<$v>\n\n";
-
-open( I, "<$ARGV[1]" ) || die; # replaced book1
-$a = <I>;
-close I;
-
-open( F, "<$ARGV[2]" ) || die; # replace control flags
-$flag = <F>;
-close F;
-
-open( O, ">$ARGV[3]" ) || die; # restored book1
-
-open( L, ">$ARGV[4]" ) || die;
-
-$l = length($a);
-#$plog = "0" x (length($a)*2);
-undef @pl;
-for( $i=0; $i<$l; $i++ ) { $pl[$i]=0; }
-
-$func2_i = 0;
-sub func2 {
-#  substr($plog,length($`),1) = '1';
-  $pl[length($`)]=1;
-  return substr($flag,$func2_i++,1) eq '1';
+sub decode_escapes {
+    my $s = shift;
+    $s =~ s/\\x([0-9A-Fa-f]{2})/chr(hex($1))/ge;
+    $s =~ s/\\t/\t/g;
+    $s =~ s/\\n/\n/g;
+    $s =~ s/\\r/\r/g;
+    $s =~ s/\\\\/\\/g;
+    return $s;
 }
 
+open my $cfg, '<:raw', $cfg_file or die "Cannot open $cfg_file: $!";
+my $cfg_data = do { local $/; <$cfg> };
+close $cfg;
 
-print "Backward replacement (book1.out+book1.flg->book1.rst)\n";
+$cfg_data =~ s/\r\n/\n/g;
+my @lines = split /\n/, $cfg_data;
 
-eval "\$a=~ s/(?<=$u0)$v(?=$u1)(?(?{func2()})|(*FAIL))/$u/g;";
+my $lb = shift @lines // '';
+my $la = shift @lines // '';
 
-print O $a;
+my %backward;
+my @backward_keys;
 
-#print L substr($plog,0,$l);
+for my $line (@lines) {
+    next if $line eq '' || $line !~ /\t/;
+    my ($from, $to) = split /\t/, $line, 2;
+    next unless defined $from && defined $to;
+    
+    $from = decode_escapes($from);
+    $to = decode_escapes($to);
+    
+    unless (exists $backward{$to}) {
+        $backward{$to} = $from;
+        push @backward_keys, $to;
+    }
+}
 
-for( $i=0; $i<$l; $i++ ) { print L chr(48+$pl[$i]); }
+die "No replacement pairs found in config\n" unless @backward_keys;
 
+my $bwd_alt = join '|', map { quotemeta } sort { length($b) <=> length($a) } @backward_keys;
 
+# Read input
+open my $in, '<:raw', $in_file or die "Cannot open $in_file: $!";
+my $data = do { local $/; <$in> };
+close $in;
+my $in_len = length($data);
+
+# Read flags
+open my $flg, '<:raw', $flg_file or die "Cannot open $flg_file: $!";
+my $flags = do { local $/; <$flg> };
+close $flg;
+
+# Collect all matches using (*FAIL) trick
+my @matches;
+my %seen_pos;
+
+sub collect_match {
+    return if $seen_pos{$-[0]}++;
+    push @matches, [$-[0], $1];
+}
+
+eval "\$data =~ /(?<=$lb)($bwd_alt)(?=$la)(?{ collect_match() })(*FAIL)/gs;";
+
+my $num_matches = scalar @matches;
+my $num_flags = length($flags);
+if ($num_matches != $num_flags) {
+    warn "Warning: Match count ($num_matches) differs from flag count ($num_flags)\n";
+}
+
+# Apply replacements left-to-right with offset tracking
+my $offset = 0;
+
+for my $i (0 .. $#matches) {
+    my $flag = $i < $num_flags ? substr($flags, $i, 1) : '0';
+    next unless $flag eq '1';
+    
+    my ($orig_pos, $match) = @{$matches[$i]};
+    my $adj_pos = $orig_pos + $offset;
+    my $repl = $backward{$match};
+    
+    substr($data, $adj_pos, length($match)) = $repl;
+    $offset += length($repl) - length($match);
+}
+
+# Write output
+open my $out, '>:raw', $out_file or die "Cannot open $out_file: $!";
+print $out $data;
+close $out;
+
+print STDERR "Input: $in_len bytes, Restored: ", length($data), " bytes, Flags used: $num_matches\n";
